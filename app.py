a010bfd4b050a8721547b834a0a9a1c8cebfcde1
motazabdou <motazabdou30@gmail.com>
2022-01-29 13:48:08 +0000

    Added base template, added views and templates for login and signup

--- app.py
+++ app.py
@@ -21,6 +21,63 @@ def home():
     pets = mongo.db.pet_types.find()
     return render_template("index.html", pets=pets)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """------------- Log in -----------------------"""
+    if request.method == "POST":
+        # check if the username already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if existing_user:
+            # check to see if hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")
+            ):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+            else:
+                # Invalid password match
+                flash("Invalid Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # Username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    """------------- Sign Up -----------------------"""
+    if request.method == "POST":
+        # Check if the username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user in a session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
             port=int(os.environ.get("PORT")),
